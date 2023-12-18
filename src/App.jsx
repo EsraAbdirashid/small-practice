88989e363f8740271e0d0a009a0a4efc9bb42f46
EsraAbdirashid <israApdirashid@gmail.com>
2023-12-18 18:54:01 +0300

    kudarr

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  
+  return (
+    <>
+    <div className="story">
+        <h1>STORY😍</h1>
+        <p id='p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, suscipit autem? Nostrum dolorem,<br />saepe in dolorum labore tempore velit explicabo, ratione veniam  voluptatibus quasi necessitatibus <br /> voluptatum quas? Obcaecati, doloribus atque!</p>
+
+        <span class="span">End👏</span>
+        </div> 
+
+    
+    </>
+  )
+}
+
+export default App
